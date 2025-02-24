a3d9c4b7c71060eca90de729d780b614694afb76
harry-upton <harryupton100@gmail.com>
2025-02-24 11:06:17 +0000

    set up endpoints with JSON validation, + some cleanup

--- flask_api/app.py
+++ flask_api/app.py
@@ -1,43 +1,43 @@
 from flask import Flask, request, jsonify, render_template
 from flask_socketio import SocketIO, emit
-
+import json
+from jsonschema import validate, ValidationError
 
 app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins='*') #Change for production!
-#foxglove = "ws://localhost:8765" #Address to connect to Foxglove
 
-@socketio.on('data_receive')
-def handle_json(json):
-    print('Received json: ' + str(json))
-    incoming_data = json
+# Load schemas from files
+with open("flask_api/schema/status_schema.json", 'r') as file:
+    status_schema = json.load(file)
+with open("flask_api/schema/log_message_schema.json", 'r') as file:
+    log_message_schema = json.load(file)
+
+# Define Endpoints
+@socketio.on("status")
+def status_endpoint(data):
+    return validate_request(data, status_schema)
+
+@socketio.on("log")
+def log_endpoint(data):
+    return validate_request(data, log_message_schema)
 
 @socketio.on('connect')
 def test_connect(message):
-    print("Success:", message)
+    print("Client connected, message:", message)
     emit("Successfully received")
 
 @socketio.on('disconnect')
 def test_disconnect(reason):
     print('Client disconnected, reason:', reason)
 
-
-@app.route("/")
-def formatted_data ():
-    formatted_data = {
-        'position': ["1.0000", "1.0000", "1.0000"],
-        'status': 1,
-        'battery': 100,
-        'dist_trav': 10,
-        #Signal quality - if required
-        'IMU': ["1.0000", "1.0000", "1.0000"],
-        'trough':
-                {
-                    'nCables': 5.0000,
-                    'areaCables': 80.0000,
-                },
-        'image': "ABC+=/akla+-" #Base 64 Image 
-    }
-    return jsonify(formatted_data)
+def validate_request(data, schema):
+    try:
+        validate(instance=data, schema=schema)
+        return {"message": "Valid request"}, 200
+    except ValidationError as e:
+        return {"error": "Invalid request", "details": e.message}, 400
+    except Exception as e:
+        return {"error": "Something went wrong", "details": str(e)}, 500
 
 if __name__ == '__main__':
     socketio.run(app, port=3000, debug=True)
--- flask_api/client.py
+++ flask_api/client.py
@@ -4,29 +4,25 @@ import json
 
 sio = socketio.Client()
 
+def send_data(endpoint, data):
+    response = sio.call(endpoint, data)
+    print(f"Response from {endpoint}: {response}")
+
 @sio.on('connect')
 def connected():
-    print("Trough Inspection Robot Connected")
+    print("Connected to server")
 
 @sio.on('disconnect')
 def disconnected():
-    print("Client Disconnected")
+    print("Disconnected from server")
 
 sio.connect('http://localhost:3000', transports=['websocket'])
 
-#sio.emit("data_receive", {"message": "testing ..."})
-sio.emit("data_receive", {
-        'position': ["1.0000", "1.0000", "1.0000"],
-        'status': 1,
-        'battery': 100,
-        'dist_trav': 10,
-        #Signal quality - if required
-        'IMU': ["1.0000", "1.0000", "1.0000"],
-        'trough':
-            {
-            'nCables': 5.0000,
-            'areaCables': 80.0000,
-            },
-        'image': "ABC+=/akla+-" #Base 64 Image 
-    })
+data =  {
+    "level": 1,
+    "message": "Test message...",
+    "timestamp": "2025-02-24T09:00:00.123Z"
+}
+send_data("log", data)
+
 sio.wait()
